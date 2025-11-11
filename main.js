4cac3eb2d23c6a0e5da7edbe1474cba042566068
kevin <kevinahholmsen@gmail.com>
2025-11-11 11:07:07 +0100

    la til index og css

---
+++ main.js
@@ -0,0 +1,14 @@
+// oppgave 1
+console.log("hello world!");
+
+// oppgave 2
+console.log("mitt navn er kevin");
+
+/*
+oppgave 3
+string concatenation er når to stringer med kode i js blir addet sammen.
+det virker ved å putte to variabler, og så f.eks plusse dem sammen.
+*/
+
+// oppgave 4
+console.log("Geir Hilmersen er født i året” + 1979 + 42 + “ år gammel!");
