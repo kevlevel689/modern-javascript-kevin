62dc7044a26bf56bd3a6d16328b4579f1a7db1bb
kevin <kevinahholmsen@gmail.com>
2025-11-18 13:42:15 +0100

    svarte oppgaver som jeg håper er korrekt

---
+++ sammensatteoperator.js
@@ -0,0 +1,134 @@
+// Oppgave 1
+// Kevin Holmsen - Oppgavesett 3b Sammensatte operatorer
+
+console.log("Kevin Holmsen - Oppgavesett 3b Sammensatte operatorer");
+
+// (1.) oppgave 2
+
+// 2. 
+let score;
+
+// 3.
+score = 10;
+
+// 5.
+score = 20;
+
+// 6.
+console.log(score); // = 20
+
+
+// (1.) oppgave 3
+
+// 2. 
+let points;
+
+// 3.
+points = 10;
+
+// 4.
+points += 5; 
+
+// 5.
+console.log(points); // = 15
+
+// 6.
+/*  For å gjøre koden ryddigere, så brukes det shortcuts til å gjøre koden litt leseligere.
+I denne eksempelen brukes "+=", som betyr at vi setter sammen to operatører
+(aritmetisk (+) og assignment (=)).
+så når vi tar points += 2;, så er det det samme som points = points + 2;. */
+
+
+
+// (1.) oppgave 4
+
+// 2. 
+let lives;
+
+// 3.
+lives = 3;
+
+// 4.
+lives -= 1; 
+
+// 5.
+console.log(lives); // = 2
+
+// (6.) 
+/* I denne eksempelen brukes "-=", som betyr at vi setter sammen to operatører
+(aritmetisk (-) og assignment (=)).
+så når vi tar lives -= 1;, så er det det samme som lives = lives - 1;. */
+
+// (1.) oppgave 5
+
+// 2. 
+let multiplier; // jeg deklarerer en ny variabel som heter "multiplier"
+
+// 3.
+multiplier = 4; // jeg initaliserer multiplier og gir den verdien 4
+
+// 4.
+multiplier *= 2; // jeg multipliserer verdien til multiplier med 2 til å få 8.
+
+// 5.
+console.log(multiplier); // = 8
+
+// (6.) (se kommentarene)
+
+
+// (1.) oppgave 6
+
+// 2. 
+let speed;
+
+// 3.
+speed = 100;
+
+// 4.
+speed /= 2 // 
+
+// 5.
+console.log(speed); // = 50
+
+// 6.
+/* /* I denne eksempelen brukes "/=", som betyr at vi setter sammen to operatører
+(aritmetisk (/) og assignment (=)).
+så når vi tar speed /= 2;, så er det det samme som speed = speed / 2;. */
+
+
+// (1.) oppgave 7
+
+// 2.
+let number;
+
+// 3.
+number = 10;
+
+// 4.
+number += 5; // 10 + 5 = 15
+
+number *= 2; // 15 * 2 = 30
+
+number -= 4; // 30 - 4 = 26
+
+number /= 2 // 26 / 2 = 13
+
+// 5.
+console.log(number); // = 13
+
+
+// oppgave 8
+
+// 1.
+let kevin; // deklarerer en variabel med navnet kevin.
+
+// 2.
+kevin = 16; // initaliserer med verdien 16.
+
+// 3.
+kevin *= 3; // 16 * 3 = 48
+
+kevin /= 5; // 48 / 5 = 9.6
+
+// 4.
+console.log(kevin); // skriver ut svaret, som er 9.6
