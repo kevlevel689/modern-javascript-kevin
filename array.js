0bf35196cc2d12fb82f14f4d5ff11be8836b82c9
kevin <kevinahholmsen@gmail.com>
2026-01-28 19:15:47 +0100

    skal gjøre ferdig resten snart

---
+++ array.js
@@ -0,0 +1,17 @@
+// oppgave 1
+// a)
+/* en Array er en liste der vi kan lagre forskjellige ting samtidig.
+Det gjør slik at vi ikke trenger å lage mange forskjellige variabler, og holder koden ryddigere. */
+
+// b)
+/* I andre programmeringsspråk så er reglene for arrays strengere, med at de trenger
+spesifike typer data og størrelse, mens på Javascript kan det inneholde forskjellige
+typer data,har metoder for å forandre det slik man vil, og fungerer som en Object. */
+
+// oppgave 2
+
+// oppgave 3
+
+// oppgave 4
+
+// oppgave 5
