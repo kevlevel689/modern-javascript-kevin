8b52c8b2048b5040453d7a197f100747d1d2b852
kevin <kevinahholmsen@gmail.com>
2026-01-28 20:56:48 +0100

    svarte på alle arrays

---
+++ arraybian.js
@@ -0,0 +1,60 @@
+// oppgave 1
+let tiere = [10, 20, 30];
+
+console.log(tiere[1]);
+
+// oppgave 2
+
+let arr = []; 
+
+arr[0] = "hello"; 
+arr[1] = "world";
+
+console.log(arr)
+
+// oppgave 3
+let tall = [1, 2, 3, 4, 5];
+console.log(tall.length)
+
+// oppgave 4
+let frukt = ["apple", "banana", "cherry"];
+
+frukt[1] = "blueberry";
+
+console.log(frukt);
+
+// oppgave 5
+let femerplasser = [5, 10, 15];
+
+femerplasser[3] = 20;
+
+console.log(femerplasser);
+
+// oppgave 6
+let farger = ["red", "green", "blue"];
+
+console.log(farger[2]);
+
+// oppgave 7
+let syvere = [7, 14, 21];
+
+console.log(syvere[0] < syvere[2]);
+
+// oppgave 8
+let toere = [2, 4, 6, 8];
+
+console.log(toere[0] + toere[2]);
+
+// oppgave 9
+let toarrays = [[1, 2, 3], [4, 5, 6]];
+
+console.log(toarrays[1][1]);
+
+// oppgave 10
+let x = 100;
+let y = 200;
+let z = 300;
+
+let verdier = [x, y, z];
+
+console.log(verdier);
