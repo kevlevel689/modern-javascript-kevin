e499311d69376cfc9931ba2cc61ca1e93e2bb966
kevin <kevinahholmsen@gmail.com>
2026-01-28 19:50:02 +0100

    faktisk svarte på alle oppgaver

--- returnstatements.js
+++ returnstatements.js
@@ -37,3 +37,61 @@ function findMax(a, b){
     }
 }
 
+// oppgave 6
+function calculateTax(inntekt) {
+    if (inntekt < 50000) {
+        return inntekt * 0,1;
+    } else if (inntekt >= 50000 && inntekt <= 100000) {
+        return inntekt * 0.2
+    } else {
+        return inntekt * 0.3
+    }
+}
+
+// oppgave 7
+function isValidEmail(email) {
+    return email.includes("@") && email.includes(".");
+}
+
+console.log(isValidEmail("test@example.com")); // true
+
+console.log(isValidEmail("testexample.com")); // false
+
+// oppgave 8
+function findLongestString(a, b) {
+    if (a.length > b.length) {
+        return a;
+    } else if (b.length > a.length) {
+        return b;
+    } else {
+        return "Strengene er like lange.";
+    }
+}
+
+// oppgave 9
+function calculateDiscount(pris, tilbudsprosent) {
+    const tilbud = pris * (tilbudsprosent / 100);
+    return pris - tilbud;
+}
+
+console.log(calculateDiscount(100, 10)); // 90
+
+console.log(calculateDiscount(200, 25)); // 150
+
+// oppgave 10
+function isStrongPassword(passord) {
+    if (passord.length < 8) return false;
+
+    if (passord.includes("0") || passord.includes("1") || passord.includes("2") ||
+        passord.includes("3") || passord.includes("4") || passord.includes("5") ||
+        passord.includes("6") || passord.includes("7") || passord.includes("8") ||
+        passord.includes("9")) {
+        return true;
+    }
+    return false;
+}
+
+console.log(isStrongPassword("abc123")); // false
+
+console.log(isStrongPassword("password1")); // true
+
