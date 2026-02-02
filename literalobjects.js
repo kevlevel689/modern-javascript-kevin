27f527426fa4672eadd631bd5fde731b5a5e5eb8
kevin <kevinahholmsen@gmail.com>
2026-02-02 22:07:56 +0100

    oppgaver er svart

---
+++ literalobjects.js
@@ -0,0 +1,80 @@
+// oppgave 1
+const elev1 = {
+  fornavn: "Anne",
+  etternavn: "Hansen",
+  alder: 17
+};
+
+const elev2 = {
+  fornavn: "Jonas",
+  etternavn: "Nilsen",
+  alder: 18
+};
+
+const elev3 = {
+  fornavn: "Ida",
+  etternavn: "Berg",
+  alder: 16
+};
+
+const elev4 = {
+  fornavn: "Martin",
+  etternavn: "Olsen",
+  alder: 17
+};
+
+// oppgave 2
+function skrivElev(elev) {
+  console.log(`Navn: ${elev.fornavn} Etternavn: ${elev.etternavn} Alder: ${elev.alder}`);
+}
+
+skrivElev(elev1);
+skrivElev(elev2);
+skrivElev(elev3);
+skrivElev(elev4);
+
+
+// oppgave 3
+function carFactory(antallSeter, hestekrefter, modell, type) {
+  return {
+    antallSeter,
+    hestekrefter,
+    modell,
+    type
+  };
+}
+
+const bil1 = carFactory(5, 150, "Focus", "sedan");
+const bil2 = carFactory(7, 200, "XC90", "SUV");
+const bil3 = carFactory(5, 110, "Golf", "stasjonsvogn");
+const bil4 = carFactory(2, 450, "911", "coupe");
+
+const biler = [bil1, bil2, bil3, bil4];
+console.log(biler);
+
+// oppgave 4
+let globalSerienummerTeller = 1000;
+
+function carFactoryMedSN(antallSeter, hestekrefter, modell, type) {
+  const car = {
+    antallSeter,
+    hestekrefter,
+    modell,
+    type,
+    serienummer: null,
+
+    genererSerienummer() {
+      globalSerienummerTeller += 1;
+      this.serienummer = `CAR-${globalSerienummerTeller}`;
+      return this.serienummer;
+    }
+  };
+
+  car.genererSerienummer(); 
+  return car; 
+}
+
+
+const bilA = carFactoryMedSN(5, 150, "Focus", "sedan");
+const bilB = carFactoryMedSN(7, 200, "XC90", "SUV");
+console.log(bilA, bilB);
--- navn.js
+++
@@ -1,9 +0,0 @@
-// oppgave 1
-
-// oppgave 2
-
-// oppgave 3
-
-// oppgave 4
-
-// oppgave 5
