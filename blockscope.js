de89cb34d543c35aafd8a6b4941df2bed7d65340
kevin <kevinahholmsen@gmail.com>
2026-02-25 23:37:10 +0100

    ryddet opp i noen kommentarer

--- blockscope.js
+++ blockscope.js
@@ -28,15 +28,18 @@ den verdien av x. */
 
 // oppgave 3
 
-const y = 10;
 
 {
 
-console.log(`y = ${y}`);
+const y = 10;
 
 }
-/* her blir utskriften en error, fordi alt innenfor en scope, dør når den er ferdig,
-så den gir en error fordi man ikke har definert y etter scopen.*/
+
+console.log(`y = ${y}`);
+
+
+/* her blir utskriften en error, fordi scopen prøvde å skrive ut en variabel som ikke hadde blitt deklarert enda, og
+den kan ikke hoiste slik funksjoner gjør.*/
 
 
 // oppgave 4
@@ -71,12 +74,12 @@ console.log(`a = ${a}`);
 
 function fubar(){
 
-a =2;
+a = 2;
 
 }
 
-/* først vil den deklarere a, og så vil den bruke funksjonen fubar til å sette a = 2.
-dermed vil console log skrive at a = 2. */
+/* først vil den deklarere a, og så vil den kalle funksjonen til å sette verdien av a.
+Etter det vil console log skrive at a = 2. */
 
 
 // oppgave 6
@@ -99,8 +102,7 @@ console.log(result);
 
 // Det gir 2 utskrifter, som er 15 og 10.
 /* Først deklarer og initaliserer vi b med verdien 10.
-og så kaller vi funksjonen add, som forandrer b til c, og 5 til d.
-etter det så deklarer og initialiserer result med c + d; som vi vet er 10 + 5, som blir 15.
-da skriver den 15 ut.
-Og så etter det, så skriver den hva b er, som er 10. */
+og så kaller vi funksjonen add, som gjør at to argumenter kan settes in.
+etter det så deklarer og initialiserer result med summen av parameterene c + d; som i dette tilfelle blir 10 + 5 = 15.
+Og så etter det, så skriver den hva verdien b er initialisert med, som er 10. */
 
